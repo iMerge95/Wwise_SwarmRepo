9aaedb5c5c2ce9aabb25d5dfad25600a7813973c
Unknown <jeremyguarober@hotmail.fr>
2018-12-19 15:15:01 +0100

    Wwise push SoundBank

---
+++ Unity/SHMUP-Swarm/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_PICKUP = 3860455926U;
+        static const AkUniqueID PLAY_SHOTS = 3061091197U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace BOSSPHASE
+        {
+            static const AkUniqueID GROUP = 2561284575U;
+
+            namespace STATE
+            {
+                static const AkUniqueID PHASE1 = 3630028971U;
+                static const AkUniqueID PHASE2 = 3630028968U;
+                static const AkUniqueID PHASE3 = 3630028969U;
+            } // namespace STATE
+        } // namespace BOSSPHASE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PYUNUMBER = 1131003326U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN_BANK = 3989171651U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
